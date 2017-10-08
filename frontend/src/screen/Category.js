883d252cd29621dee25840e92511fbb0d42539aa
Jasper Stam <jasper@codeyellow.nl>
2017-10-08 21:33:08 +0200

    Add category add/edit form

--- frontend/src/screen/Category.js
+++ frontend/src/screen/Category.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import { CategoryStore } from '../store/Category';
+import { CategoryStore, Category } from '../store/Category';
+import CategoryEdit from '../container/Category/Edit';
 import View from '../store/View';
 import Level from '../container/Category/Level';
 import { Body, Heading, Row } from 're-cy-cle';
@@ -9,16 +10,26 @@ import { Body, Heading, Row } from 're-cy-cle';
 @observer
 export default class CategoryScreen extends Component {
     static propTypes = {
+        match: PropTypes.object.isRequired,
         viewStore: PropTypes.instanceOf(View).isRequired,
     };
 
     componentWillMount() {
+        const id = this.props.match.params.id;
+        console.log('match.params', this.props.match.param);
+
         this.categoryStore = new CategoryStore();
+        this.category = new Category({ id: id ? parseInt(id) : null });
     }
 
     componentDidMount() {
         this.categoryStore.fetch();
     }
+
+    handleCreate = c => {
+        this.categoryStore.add(c.toJS());
+        c.clear();
+    };
 
     render() {
         return (
@@ -26,6 +37,12 @@ export default class CategoryScreen extends Component {
                 <Heading>Categories</Heading>
                 <Row>
                     <Level height={0} store={this.categoryStore} />
+                </Row>
+                <Row>
+                    <CategoryEdit
+                        onCreate={this.handleCreate}
+                        model={this.category}
+                    />
                 </Row>
             </Body>
         );
