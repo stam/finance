6d64b037a04e4fdceed06cd0c63c01ffdf84d871
Jasper Stam <jasper@codeyellow.nl>
2017-10-15 20:42:06 +0200

    Add name and category to query model

--- backend/base/models/query.py
+++ backend/base/models/query.py
@@ -1,11 +1,15 @@
 from django.db import models
 from binder.models import BinderModel
+from mptt.models import TreeForeignKey
+
 from django.contrib.postgres.fields import JSONField
 
 
 class Query(BinderModel):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='queries')
+    category = TreeForeignKey('Category', related_name='queries', on_delete=models.CASCADE)
 
+    name = models.TextField()
     matcher = JSONField(default={})
     disabled = models.BooleanField(default=False)
 
