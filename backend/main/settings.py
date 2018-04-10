041ebe0f600fd92ea442a61759838b7bbe8d145e
Jasper Stam <jasper@codeyellow.nl>
2018-04-10 22:04:13 +0200

    Fix parse_bool in settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dotenv
 
+
+def parse_bool(value):
+    return {'true': True, 'false': False}[str(value).lower()]
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
