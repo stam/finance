5563fe094a7db1a65a8980021016c0a8dc6b2f5b
Jasper Stam <jasper@strnk.nl>
2019-12-24 12:54:13 +0100

    🏦 Add budget

---
+++ backend/base/models/budget.py
@@ -0,0 +1,13 @@
+from binder.models import BinderModel
+
+from django.db import models
+
+
+class Budget(BinderModel):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='budgets')
+
+    name = models.TextField()
+    amount = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
