70c5acb5eef1be7a06acac1e871eae60dc6806e8
Jasper Stam <jasper@strnk.nl>
2020-01-08 22:41:06 +0100

    ⚛️  Bootstrap new frontend

---
+++ frontend-v2/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Frontend v2</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
