bf7decd04c97c0cfd23d5fb35feb028401d245f2
Jasper Stam <jasper@strnk.nl>
2020-02-23 17:57:10 +0100

    💏 Add date grouping to transaction view

--- frontend-v2/src/screens/Transactions.tsx
+++ frontend-v2/src/screens/Transactions.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, Fragment } from "react";
 import { observer } from "mobx-react-lite";
 import styled from "styled-components";
+import { map } from "lodash";
 
 import { Header } from "../components/Header";
 import { TransactionItem } from "../components/Transaction";
 import { Nav } from "../components/Nav";
 import { TransactionStore } from "../store/Transaction";
+import moment from "moment";
 
 const Container = styled.div`
   display: flex;
@@ -13,6 +15,12 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   overflow: hidden;
+`;
+
+const DateHeader = styled.p`
+  margin-left: 1rem;
+  color: black;
+  text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.5);
 `;
 
 const Overview = styled.div`
@@ -26,10 +34,24 @@ export const Transactions: React.FC = observer(() => {
       relations: ["category"]
     })
   );
+  const [currentYear] = useState(moment().year());
 
   const fetchData = useCallback(() => {
     transactionStore.fetch();
   }, [transactionStore]);
+
+  const formatDate = (date: moment.Moment) => {
+    if (date.year() < currentYear) {
+      return date.format("dddd DD MMM YYYY");
+    } else {
+      return date.calendar(undefined, {
+        sameDay: "[Today]",
+        lastDay: "[Yesterday]",
+        lastWeek: "[Last] dddd",
+        sameElse: "dddd DD MMM"
+      });
+    }
+  };
 
   useEffect(() => {
     fetchData();
@@ -39,8 +61,13 @@ export const Transactions: React.FC = observer(() => {
     <Container>
       <Header>Transactions</Header>
       <Overview>
-        {transactionStore.models.map(t => (
-          <TransactionItem key={t.id} model={t} />
+        {map(transactionStore.groupByDate, (val, date) => (
+          <Fragment>
+            <DateHeader>{formatDate(val[0].date)}</DateHeader>
+            {val.map(t => (
+              <TransactionItem key={t.id} model={t} />
+            ))}
+          </Fragment>
         ))}
       </Overview>
       <Nav />
