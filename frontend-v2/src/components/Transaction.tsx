3cbc6b76b17d316297947bdab3e4f41980438483
Jasper Stam <j.stam@vanberlo.nl>
2020-10-04 00:29:32 +0200

    🚁 Add expanded transaction state which shows more data

--- frontend-v2/src/components/Transaction.tsx
+++ frontend-v2/src/components/Transaction.tsx
@@ -16,26 +16,41 @@ const Container = styled.div`
   align-items: center;
   grid-column-gap: 0.5rem;
   grid-template-columns: 2.5rem auto 4rem;
+  border: 3px solid white;
+  border-width: 1px 3px 3px 1px;
+  cursor: pointer;
+
+  &:hover {
+    border-color: rgba(0, 0, 0, 0.1);
+  }
 `;
 
 const Title = styled.p``;
+
+const Description = styled.p`
+  grid-column: 1 / -1;
+`;
 
 interface TransactionProps {
   model: Transaction;
 }
 
-export const TransactionItem: React.FC<TransactionProps> = observer(props => {
+export const TransactionItem: React.FC<TransactionProps> = observer((props) => {
   const { model } = props;
   const [active, setActive] = useState(false);
+  const [expanded, expand] = useState(false);
+
+  console.log(model);
 
   return (
-    <Container>
+    <Container onClick={() => expand(!expanded)}>
       <CategoryIcon type={model.categoryName} onClick={() => setActive(true)} />
       {active && (
         <CategorySelect model={model} close={() => setActive(false)} />
       )}
       <Title>{model.summary}</Title>
       <Amount>{model.amount}</Amount>
+      {expanded && <Description>{model.details}</Description>}
     </Container>
   );
 });
