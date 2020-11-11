485977cb125afa2e06a45371264bda80b95df800
Jasper Stam <j.stam@vanberlo.nl>
2020-11-11 18:15:27 +0100

    ↩️  Add nginx reload to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ cd frontend-v2 && yarn && yarn build
 cd ..
 docker-compose --env-file .env.production -f docker-compose.prod.yml up -d --build
 docker exec -it finance_web_1 python manage.py migrate
+docker exec -it nginx nginx -s reload
